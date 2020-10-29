e029716ce247c8a5da2b4b0270f5f077309412ba
Joonsoo Kim <daniel.kim365@hotmail.com>
2020-10-29 12:30:02 -0400

    finalizied

---
+++ src/api/User/me/me.js
@@ -0,0 +1,14 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+
+export default {
+  Query: {
+    me: async (_, __, { request, isAuthenticated }) => {
+      console.log("user parent", _)
+      isAuthenticated(request);
+      const { user } = request;
+      return await prisma.user({ id: user.id });
+     
+    }
+  },
+};
